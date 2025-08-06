95d8a2f479d3557a62e1e29dbd26bb29ce9618af
GastonYasuda <gastonyasuda@gmail.com>
2025-08-05 23:07:41 -0300

    create saveDataInfo

--- src/Component/ShowByStore/ShowByStore.jsx
+++ src/Component/ShowByStore/ShowByStore.jsx
@@ -5,6 +5,8 @@ import ShiftList from '../ShiftList/ShiftList';
 import { useContext } from 'react';
 import { apiEmployee } from '../../Context/EmployeeApiContext';
 import { useParams } from 'react-router-dom';
+import { FaSave } from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 
 
@@ -16,6 +18,16 @@ const ShowByStore = ({ employee }) => {
 
     const myNewDateId = new Date(byDateId).toLocaleDateString("es-ES", { weekday: "long" })
 
+    const saveDateInfo = () => {
+        const txt =
+            <div>
+                TODAVÍA NO ANDA ESTA FUNCIÓN!<br />
+                Pero se va a guardar la info de<br /><strong>{storeId}</strong> de la fecha <strong>{byDateId}</strong>
+            </div>;
+
+        toast(txt)
+
+    }
 
     return (
         <>
@@ -24,6 +36,13 @@ const ShowByStore = ({ employee }) => {
 
                 <h3>{mayPrimera(storeId)}</h3>
                 <h6>{mayPrimera(myNewDateId)} {byDateId}</h6>
+
+
+                <button style={{ all: 'unset' }} onClick={() => { saveDateInfo() }}>
+                    <div style={{ textAlign: 'right', fontSize: '1.8rem' }}>
+                        <FaSave color="white" />
+                    </div>
+                </button>
 
 
                 <ScheduleGraphic employeeData={employee} />
